29e44b251f7a1d6329d961a0ae7be697ea9c4f6e
GrzegorzNiewierowski <gniewierowski@gmail.com>
2022-03-31 19:36:34 +0200

    displaying books

--- main/models.py
+++ main/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+
 class Book(models.Model):
     tytuł = models.CharField(max_length=200)
     cena = models.DecimalField(max_digits=10, decimal_places=2)
-    zdjęcie = models.ImageField(upload_to="main/static/main/images/")
+    zdjęcie = models.ImageField(upload_to=f'books_covers/')
+    opis = models.TextField()
+    autor = models.CharField(max_length=200)
+    data_publikacji = models.DateField(null=True)
 
     def __str__(self):
         return self.tytuł
