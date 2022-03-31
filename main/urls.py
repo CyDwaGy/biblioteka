9ed69684c0c2983a0968ef8e0d337392e8ce1ce3
GrzegorzNiewierowski <gniewierowski@gmail.com>
2022-03-31 22:15:56 +0200

    Page for showing items

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('item/<int:id>', views.item, name='item')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -6,3 +6,8 @@ def index(request):
     Books_list = Book.objects.all()
     context = {'Books_list': Books_list}
     return render(request, 'main/index.html', context)
+
+def item(request, id):
+    book = Book.objects.get(id=id)
+    context = {'book': book}
+    return render(request, 'main/item.html', context)
