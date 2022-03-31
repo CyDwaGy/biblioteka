246c4896c286e3c5793d3b0c76c48e1674ed2ecc
GrzegorzNiewierowski <gniewierowski@gmail.com>
2022-03-31 23:41:32 +0200

    Change variables names to english

--- main/models.py
+++ main/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Book(models.Model):
-    tytuł = models.CharField(max_length=200)
-    cena = models.DecimalField(max_digits=10, decimal_places=2)
-    zdjęcie = models.ImageField(upload_to=f'books_covers/')
-    opis = models.TextField()
-    autor = models.CharField(max_length=200)
-    data_publikacji = models.DateField(null=True)
+    title = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    picture = models.ImageField(upload_to=f'books_covers/', blank=True)
+    desc = models.TextField()
+    author = models.CharField(max_length=200)
+    pub_date = models.DateField(null=True)
 
     def __str__(self):
-        return self.tytuł
+        return self.title
